e5f4ea828b38332eec794a77380dc033a875fc37
Gian Neves <gianneves@hotmail.com>
2023-11-03 14:21:16 -0300

    feat: Create project

---
+++ src/main/java/com/gianneves/filestoregeapi/FileStoregeApiApplication.java
@@ -0,0 +1,13 @@
+package com.gianneves.filestoregeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStoregeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStoregeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gianneves/filestoregeapi/FileStoregeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianneves.filestoregeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStoregeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
